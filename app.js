474731234c2671ef613fccdf47e9b1a79d6d12ff
Flavio_Zanoni <flaviozg888@gmail.com>
2021-04-15 09:17:21 -0300

    started the first page game
    added an dummy sprite, changed the css to support the game window, added the simple code for the game, chamed the script tag to only load after the html is fully loaded

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = new PIXI.Application({
+    width: 880,
+    height:495
+});
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.querySelector("#gamediv").appendChild(app.view);
+
+// load the texture we need
+app.loader.add('bunny', '../images/sprites/mugi2.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(() => {
+         // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+});
