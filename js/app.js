65a8833f68491bf161a01f656f065e519ba5c33f
Flavio_Zanoni <flaviozg888@gmail.com>
2021-05-11 20:42:58 -0300

    Shaders and sprite movement
    Started modifying the shader, added the values to be changed, only left is the changer based on the time of the day, made a simple movement for the sprite on the A,W,S,D keys, changed the code to support the new background image

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
 //imports the noise filter
-import noise from '../js/shader.js';
+import time from '../js/shader.js';
 
 // select the scale mode to nearest bc the game is in pixelart
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+ 
 
 //global declarations
 let w = window.innerWidth;
 let h = window.innerHeight;
-// gets data and time
-let date = new Date();
-let hours = date.getHours();
 //sprites container
 let sprites = [];
 // containers
-let startGame = new PIXI.Container()
-let scenary = new PIXI.Container()
-
+let startGame = new PIXI.Container();
+let scenary = new PIXI.Container();
 
 //creates the app instance
 const app = new PIXI.Application({
@@ -23,133 +20,121 @@ const app = new PIXI.Application({
     height: h
 });
 
+
 //create the canvas on the #gamediv div
 document.querySelector("#gamediv").appendChild(app.view);
 
 //loaders
 const loader = PIXI.Loader.shared;
-loader.add('farMountains', '../images/sprites/farmountains.png');
-loader.add('mountains', '../images/sprites/mountains.png');
-loader.add('hills', '../images/sprites/hills.png');
-loader.add('plants', '../images/sprites/plants.png');
+
+loader.add('background', '../images/sprites/background.png')
+loader.add('sky', '../images/sprites/sky.png');
 loader.add('tree', '../images/sprites/tree.png');
 loader.add('ground', '../images/sprites/ground.png');
-
+loader.add('player', '../images/sprites/armored.png');
 
 loader.onComplete.add(loaded);
-function loaded() {
-    console.log('f');
-    app.stage.addChild(scenary)
-    scenary.filters = [noise]
-}
-/*
-//load the right image depending on the date
-//checks for morning (6am to 12am) afternoon (12 to 17) evening (17 to 20) night(20 to 6)
-if(hours > 6 && hours < 12){
-    //load moutains
-    app.loader.add('mountains', '../images/sprites/morning/morning.jpg')
-    //load terrain
-    app.loader.add('ground')
-    //load sakura
-    app.loader.add('sakura')
-    //load player
-    app.loader.add('player')
-    //load tree leaves
-    app.loader.add('leaves')
-    //load nav
-    app.loader.add('nav')
-} else if (hours >= 12 && hours < 17) {
-    //load moutains
-    app.loader.add('mountains','../images/sprites/afternoon/afternoon.jpg')
-    //load terrain
-    app.loader.add('ground')
-    //load sakura
-    app.loader.add('sakura')
-    //load player
-    app.loader.add('player')
-    //load tree leaves
-    app.loader.add('leaves')
-    //load nav
-    app.loader.add('nav')
-} else if (hours >= 17 && hours <   0) {
-    //load moutains
-    app.loader.add('mountains', '../images/sprites/evening/evening.jpg')
-     //load terrain
-    app.loader.add('ground')
-    //load sakura
-    app.loader.add('sakura')
-    //load player
-    app.loader.add('player')
-    //load tree leaves
-    app.loader.add('leaves')
-    //load nav
-    app.loader.add('nav')
-} else {
-    //load moutains
-    app.loader.add('mountains', '../images/sprites/night/night.jpg')
-    //load terrain
-    app.loader.add('ground')
-    //load sakura
-    app.loader.add('sakura')
-    //load player
-    app.loader.add('player')
-    //load tree leaves
-    app.loader.add('leaves')
-    //load nav
-    app.loader.add('nav')
-}
-*/
-loader.load((loader, resources) => {
 
+function loaded() {
+    console.log('[app] => Loaded');
+    scenary.filters = [time];
+    app.stage.addChild(scenary);
+    createPlayer();
+    app.ticker.add(gameLoop);
+}
+
+loader.load((loader, resources) => {
+    
     // adding the sprites picked from the loader
-    sprites.farMountains = PIXI.Sprite.from(loader.resources.farMountains.texture);
-    sprites.mountains = PIXI.Sprite.from(loader.resources.mountains.texture);
-    sprites.hills = PIXI.Sprite.from(loader.resources.hills.texture);
-    sprites.plants = PIXI.Sprite.from(loader.resources.plants.texture);
-    sprites.tree = PIXI.Sprite.from(loader.resources.tree.texture)
-    sprites.ground = PIXI.Sprite.from(loader.resources.ground.texture)
+    sprites.background = PIXI.Sprite.from(loader.resources.background.texture)
+    sprites.tree = PIXI.Sprite.from(loader.resources.tree.texture);
+    sprites.ground = PIXI.Sprite.from(loader.resources.ground.texture);
+    sprites.sky = PIXI.Sprite.from(loader.resources.sky.texture);
+    //sky set
+    scenary.addChild(sprites.sky);
+    sprites.sky.width = w;
+    sprites.sky.height = h;
     
-    //farMoutains set
-    scenary.addChild(sprites.farMountains)
-    sprites.farMountains.width = w
-    sprites.farMountains.height = h
-    
-    //moutains set
-    scenary.addChild(sprites.mountains)
-    sprites.mountains.width = w
-    sprites.mountains.height = h
-    
-    //hills set
-    scenary.addChild(sprites.hills)
-    sprites.hills.width = w
-    sprites.hills.height = h
-    
-    //plants set
-    scenary.addChild(sprites.plants)
-    sprites.plants.width = w
-    sprites.plants.height = h
+    // backgorund set
+    scenary.addChild(sprites.background);
+    sprites.background.width = w;
+    sprites.background.height = h;
     
     //tree set
-    scenary.addChild(sprites.tree)
-    sprites.tree.width = w
-    sprites.tree.height = h
+    scenary.addChild(sprites.tree);
+    sprites.tree.width = w;
+    sprites.tree.height = h;
     
     //ground set
-    scenary.addChild(sprites.ground)
-    sprites.ground.width = w
-    sprites.ground.height = h
+    scenary.addChild(sprites.ground);
+    sprites.ground.width = w;
+    sprites.ground.height = h;
+    
     // needs to load the leaves falling
     // needs to load the player 
     // needs to load the interactive parts
-    // checar horario, cada horario um cenario com iluminaçao e vibe diferente
-    /* 
-        serão geradas 4 cenarios:
-        manha - sol nascendo, iluminaçao amarelada/laranjada, vento calmo, vibe de tempo fresco
-        tarde - sol em no topo, iluminaçao mais azulada, vento mais forte, vibe de tempo mais quente
-        fim de tarde - sol no canto, iluminaçao laranjada, vendo forte, vibe de tempo ameno
-        noite, lua no céu, vento forte, vibe bem fresca
-    */
 });
+
+function createPlayer() {
+    sprites.player = PIXI.Sprite.from(loader.resources.player.texture);
+    // player set
+    sprites.player.width = w / 16;
+    sprites.player.height = h / 10;
+    sprites.player.x = app.view.width / 2;
+    sprites.player.y = app.view.height / 2; 
+    app.stage.addChild(sprites.player);
+}
+
+// lsiteners and movement
+let keys = {};
+window.addEventListener("keydown", (e) => { keys[e.key] = true });
+window.addEventListener("keyup", (e) => { keys[e.key] = false});
+
+function gameLoop() {
+    // w
+    if (keys["w"]) {
+        /* if (!player.playing) {
+            player.textures = playerSheet.walkNorth;
+            player.play();
+        } */
+        sprites.player.y -= 5;
+    }
+    // a
+    if (keys["a"]) {
+        /*  if (!player.playing) {
+            player.textures = playerSheet.walkWest;
+            player.play();
+        } */
+        sprites.player.x -= 5;
+    }
+    // s
+    if (keys["s"]) {
+        /* if (!player.playing) {
+            player.textures = playerSheet.walkSouth;
+            player.play();
+        } */
+        sprites.player.y += 5;
+    }
+    // d
+    if (keys["d"]) {
+        /* if (!player.playing) {
+            player.textures = playerSheet.walkEast;
+            player.play();
+        } */
+        sprites.player.x += 5;
+    }
+}
+
+
+// how to animate?
+var renderer = new PIXI.autoDetectRenderer(w, h);
+function animate() {
+    // start the timer for the next animation loop
+    requestAnimationFrame(animate);
+    // this is the main render call that makes pixi draw your container and its children.
+    renderer.render(scenary);
+}
+animate()
 
 
 
--- js/shader.js
+++ js/shader.js
@@ -13,19 +13,106 @@ let shader = {
     fragShader: `
         varying vec2 vTextureCoord;
         uniform sampler2D uSampler;
-        uniform float delta;
+        uniform float red;
+        uniform float green;
+        uniform float blue;
+        uniform float exposure;
+        uniform float con;
+
         void main(void){
             vec4 color = texture2D(uSampler, vTextureCoord);
             if (color.a != 0.0){
-                color.r = delta;
-                color.g -= delta;
-                color.b -= delta;
+                color.r += red;
+                color.g += green;
+                color.b += blue;
+                color = (1.0 + exposure) * color;
+                color =  0.5 + (1.0 + con) * (color - 0.5);
             }
             gl_FragColor = color;
         }
     `,
     delta: 0
 }
-const noise = new PIXI.Filter(shader.vertShader, shader.fragShader, shader.delta);
 
-export default noise
+//why exposure?
+/**
+ This is similar to brightness, but the change of brightness is proportional to the luminosity of the colours. In other words, we have to multiply instead of add. We have to use 1 as the base value for the exact same reason we had to for contrast.
+ */
+
+let x = 10  // divisive value to amenize the color temperature
+let date = new Date();
+let hours = date.getHours();
+let rr = (255 / x) / 255;
+let gg = (250 / x) / 255;
+let bb = (230 / x) / 255;
+let ex = 50 / 255;
+let con = .2
+
+// meio dia -
+// tarde -
+// anoitecer -
+// noite -
+// manha -
+
+
+/* if (hours < 12 && hours > 6){
+    for (let c = 0; hours > c; c++){
+        // rr += 1
+        // gg = ?
+        // bb = ?
+    }
+} else if (hours > 12 && hours < 18){
+    for (let c = 0; hours > c; c++){
+        // rr = ?
+        // gg = ?
+        // bb = ?
+    }
+} else if (hours > 18 && hours < 20){
+    for (let c = 0; hours > c; c++){
+        // rr = ?
+        // gg = ?
+        // bb = ?
+    }
+} else {
+    for (let c = 0; hours > c; c++){
+        // rr = ?
+        // gg = ?
+        // bb = ?
+    }
+} */
+let uniforms = {
+    red: rr,
+    green: gg,
+    blue: bb,
+    exposure: ex,
+    con: con
+}
+
+const time = new PIXI.Filter(shader.vertShader, shader.fragShader, uniforms);
+
+export default time
+
+// ideias 
+/*
+    shader de luz para o sol e a lua
+    em vez de fazer varios desenhos a ideia agora é mudar as cores do desenho em relaçao ao ao horario do dia, desse modo nao preciso fazer desenhos, posso mudar matematicamente os pixels
+    ate agora tenho:
+    - fazer 3 containers variaveis para R G B
+    - cada container vai ter uma fase diferente do outro de acordo com a mudança das cores no dia
+    - provavelmente tem que acabar em um ciclo, de manha é mais frio, de tarde esquenta as cores, de noite começa a esfriar, de manha retorna o ciclo
+    - talvez mudar os sprides ate agora, o jeito que estao agora ele esta levando em consideraçao uma fonte de luz da direita, assim nao mudando o lugar do sol nunca
+    talvez o melhor é fazer 8 desenhos para cada faze do sol e da lua e seguir a vida ou fazer de um modo que o desenho nao tenha em si nenhuma luz direcional
+    - estudar as cores do dia para ver como variar os containers
+
+    shader de tv antiga ainda é real, tem que estudar mais em como fazer
+
+*/
+
+// essa ideia ainda mantem de referencia 
+/* 
+    serão geradas 4 cenarios:
+    manha - sol nascendo, iluminaçao amarelada/laranjada, vento calmo, vibe de tempo fresco
+    tarde - sol em no topo, iluminaçao mais azulada, vento mais forte, vibe de tempo mais quente
+    fim de tarde - sol no canto, iluminaçao laranjada, vendo forte, vibe de tempo ameno
+    noite, lua no céu, vento forte, vibe bem fresca
+*/
