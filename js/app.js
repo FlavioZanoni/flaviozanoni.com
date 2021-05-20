38a130212019f7ab52a43ac280e052204cd4b7a8
Flavio_Zanoni <flaviozg888@gmail.com>
2021-05-20 18:57:28 -0300

    added the music player
    added the music player along with the musics and wind sound

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ import time from '../js/shader.js';
 // select the scale mode to nearest bc the game is in pixelart
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
  
-
 //global declarations
 let w = window.innerWidth;
 let h = window.innerHeight;
+let rotation = 0.05;
 //sprites container
 let sprites = [];
 // containers
 let startGame = new PIXI.Container();
 let scenary = new PIXI.Container();
+let mPlayer = new PIXI.Container();
 
 //creates the app instance
 const app = new PIXI.Application({
@@ -20,18 +21,40 @@ const app = new PIXI.Application({
     height: h
 });
 
-
 //create the canvas on the #gamediv div
 document.querySelector("#gamediv").appendChild(app.view);
 
-//loaders
+//loader
 const loader = PIXI.Loader.shared;
 
-loader.add('background', '../images/sprites/background.png')
+loader.add('background', '../images/sprites/background.png');
 loader.add('sky', '../images/sprites/sky.png');
 loader.add('tree', '../images/sprites/tree.png');
 loader.add('ground', '../images/sprites/ground.png');
 loader.add('player', '../images/sprites/armored.png');
+
+loader.add('tray', "../images/musicPlayer/tray.png");
+loader.add('next', "../images/musicPlayer/next.png");
+loader.add('prev', "../images/musicPlayer/prev.png");
+loader.add('nextClicked', "../images/musicPlayer/nextClicked.png");
+loader.add('prevClicked', "../images/musicPlayer/prevClicked.png");
+loader.add('play', "../images/musicPlayer/play.png");
+loader.add('disk', "../images/musicPlayer/disk.png");
+
+loader.load((loader) => {
+    // adding the sprites picked from the loader
+    sprites.background = PIXI.Sprite.from(loader.resources.background.texture)
+    sprites.tree = PIXI.Sprite.from(loader.resources.tree.texture);
+    sprites.ground = PIXI.Sprite.from(loader.resources.ground.texture);
+    sprites.sky = PIXI.Sprite.from(loader.resources.sky.texture);
+    sprites.player = PIXI.Sprite.from(loader.resources.player.texture);
+    // mplayer
+    sprites.tray = PIXI.Sprite.from(loader.resources.tray.texture);
+    sprites.next = PIXI.Sprite.from(loader.resources.next.texture);
+    sprites.prev = PIXI.Sprite.from(loader.resources.prev.texture);
+    sprites.play = PIXI.Sprite.from(loader.resources.play.texture);
+    sprites.disk = PIXI.Sprite.from(loader.resources.disk.texture);
+});
 
 loader.onComplete.add(loaded);
 
@@ -39,50 +62,198 @@ function loaded() {
     console.log('[app] => Loaded');
     scenary.filters = [time];
     app.stage.addChild(scenary);
+    app.stage.addChild(mPlayer);
     createPlayer();
+    createScenary();
+    createMPlayer();
     app.ticker.add(gameLoop);
 }
 
-loader.load((loader, resources) => {
-    
-    // adding the sprites picked from the loader
-    sprites.background = PIXI.Sprite.from(loader.resources.background.texture)
-    sprites.tree = PIXI.Sprite.from(loader.resources.tree.texture);
-    sprites.ground = PIXI.Sprite.from(loader.resources.ground.texture);
-    sprites.sky = PIXI.Sprite.from(loader.resources.sky.texture);
+function createScenary() {
     //sky set
-    scenary.addChild(sprites.sky);
     sprites.sky.width = w;
     sprites.sky.height = h;
+    scenary.addChild(sprites.sky);
     
     // backgorund set
-    scenary.addChild(sprites.background);
     sprites.background.width = w;
     sprites.background.height = h;
+    scenary.addChild(sprites.background);
     
     //tree set
-    scenary.addChild(sprites.tree);
     sprites.tree.width = w;
     sprites.tree.height = h;
+    scenary.addChild(sprites.tree);
     
     //ground set
-    scenary.addChild(sprites.ground);
     sprites.ground.width = w;
     sprites.ground.height = h;
-    
-    // needs to load the leaves falling
-    // needs to load the player 
-    // needs to load the interactive parts
-});
-
+    scenary.addChild(sprites.ground);
+}
 function createPlayer() {
-    sprites.player = PIXI.Sprite.from(loader.resources.player.texture);
     // player set
     sprites.player.width = w / 16;
     sprites.player.height = h / 10;
     sprites.player.x = app.view.width / 2;
     sprites.player.y = app.view.height / 2; 
     app.stage.addChild(sprites.player);
+}
+function createMPlayer() {
+
+    mPlayer.x = w - 200
+    mPlayer.y = h - 900
+
+    mPlayer.addChild(sprites.tray);
+    sprites.tray.width = 120;
+    sprites.tray.height = 45;
+    
+    //next
+    mPlayer.addChild(sprites.next);
+    sprites.next.width = 25;
+    sprites.next.height = 25;
+    sprites.next.interactive = true;
+    sprites.next.buttonMode = true;
+    sprites.next.on('pointerdown', nextTrack);
+    
+    //prev
+    mPlayer.addChild(sprites.prev);
+    sprites.prev.width = 25;
+    sprites.prev.height = 25;
+    sprites.prev.interactive = true;
+    sprites.prev.buttonMode = true;
+    sprites.prev.on('pointerdown', backTrack)
+    
+    //play / pause
+    mPlayer.addChild(sprites.play);
+    sprites.play.width = 25;
+    sprites.play.height = 25;
+    sprites.play.interactive = true;
+    sprites.play.buttonMode = true;
+    sprites.play.on('pointerdown', demute);
+    
+    mPlayer.addChild(sprites.disk);
+    sprites.disk.width = 60;
+    sprites.disk.height = 60;
+    sprites.disk.anchor.set(0.5);
+    sprites.disk.interactive = true;
+    sprites.disk.buttonMode = true;
+    sprites.disk.on('pointerdown', redirect);
+
+    sprites.tray.x = -20
+    sprites.tray.y = -10
+    
+    sprites.prev.x = -5;
+    sprites.prev.y = 2;
+
+    sprites.play.x = 22;
+    sprites.play.y = 2;
+
+    sprites.next.x = 50;
+    sprites.next.y = 2;
+
+    sprites.disk.x = 110;
+    sprites.disk.y = 15;
+}
+
+let wind = new Howl ({
+    src:['../sounds/windo.mp3'],
+    autoplay: true,
+    volume: .3,
+    loop:true
+})
+
+
+let tracks = [
+    new Howl({
+        src: ['../sounds/music/Andr3w-Planet-140-BPM.mp3'],
+        volume: 0.4,
+        loop: true
+        /* skip the song when it is over
+        onend: () => {
+            nextTrack()
+        } */
+    }),
+    new Howl({
+        src: ['../sounds/music/Andr3w-Cloud-140-BPM.mp3',],
+        volume: 0.4,
+        loop: true
+        /* onend: () => {
+            nextTrack()
+        } */
+    }),
+    new Howl({
+        src: ['../sounds/music/Andr3w-Fun-147-BPM.mp3'],
+        volume: 0.4,
+        loop: true
+        /* onend: () => {
+            nextTrack()
+        } */
+    }),
+    new Howl({
+        src: ['../sounds/music/Andr3w-Chill-140-BPM.mp3'],
+        volume: 0.4,
+        loop: true
+        /* onend: () => {
+            nextTrack()
+        } */
+    })
+];
+
+let trackNum = 0;
+let trackMax = tracks.length - 1;   
+let paused = false;
+console.log(trackMax);
+tracks[trackNum].play();
+
+// mPlayer commands
+function nextTrack() {
+    console.log('[music] => Next track');
+    if (trackNum == trackMax) {
+        tracks[trackMax].stop();
+        trackNum = 0;
+        tracks[trackNum].play();
+        console.log(trackNum);
+
+    } else {
+        tracks[trackNum].stop();
+        trackNum++;
+        tracks[trackNum].play();
+        console.log(trackNum);
+    }
+}
+
+function backTrack() {
+    console.log('[music] => Back track');
+    if (trackNum == 0) {
+        tracks[trackNum].stop();
+        trackNum = trackMax;
+        tracks[trackNum].play();
+    } else {
+        tracks[trackNum].stop();
+        trackNum--;
+        tracks[trackNum].play();
+    }
+}
+
+function demute() {
+    let saveseek;
+    if (tracks[trackNum].playing()){
+        paused = true;
+        tracks[trackNum].pause();
+        rotation = 0;
+        saveseek = tracks[trackNum].seek();
+        console.log('[music] => Paused');
+    } else if (paused == true) {
+        tracks[trackNum].play();
+        tracks[trackNum].seek(saveseek);
+        rotation = 0.05;
+        console.log('[music] => Unpaused');
+    }
+}
+
+function redirect() {
+    console.log('redirect');
+    window.open("https://soundcloud.com/user-102055220");
 }
 
 // lsiteners and movement
@@ -91,6 +262,8 @@ window.addEventListener("keydown", (e) => { keys[e.key] = true });
 window.addEventListener("keyup", (e) => { keys[e.key] = false});
 
 function gameLoop() {
+    sprites.disk.rotation += rotation;
+
     // w
     if (keys["w"]) {
         /* if (!player.playing) {
@@ -124,18 +297,3 @@ function gameLoop() {
         sprites.player.x += 5;
     }
 }
-
-
-// how to animate?
-var renderer = new PIXI.autoDetectRenderer(w, h);
-function animate() {
-    // start the timer for the next animation loop
-    requestAnimationFrame(animate);
-    // this is the main render call that makes pixi draw your container and its children.
-    renderer.render(scenary);
-}
-animate()
-
-
-
-
