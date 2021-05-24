183b42cb3ecc7a6c697c8ccc567d30ec009ef35d
FlavioZanoni <flaviozg888@gmail.com>
2021-05-24 11:51:37 -0300

    Made the collision and movement
    Made the collision and moviment, changed the way it will render the game, now the scaling will be in css, so made some changes in the css

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ import time from '../js/shader.js';
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
  
 //global declarations
-let w = window.innerWidth;
-let h = window.innerHeight;
+let w = 512;
+let h = 288;
+let windoww = window.innerWidth;
+let windowh = (window.innerHeight - 15);
 let rotation = 0.05;
 //sprites container
 let sprites = [];
@@ -21,6 +23,14 @@ const app = new PIXI.Application({
     height: h
 });
 
+//scale the image acording to the size of the screen
+let scalew = windoww / w;
+let scaleh = windowh / h;
+//scaleh = Math.floor(scaleh * 100 ) / 100
+document.querySelector("#gamediv").style.transform = `scale(${scalew}, ${scaleh})`;
+
+
+
 //create the canvas on the #gamediv div
 document.querySelector("#gamediv").appendChild(app.view);
 
@@ -31,7 +41,7 @@ loader.add('background', '../images/sprites/background.png');
 loader.add('sky', '../images/sprites/sky.png');
 loader.add('tree', '../images/sprites/tree.png');
 loader.add('ground', '../images/sprites/ground.png');
-loader.add('player', '../images/sprites/armored.png');
+loader.add('player', '../images/sprites/player.png');
 
 loader.add('tray', "../images/musicPlayer/tray.png");
 loader.add('next', "../images/musicPlayer/next.png");
@@ -67,8 +77,8 @@ function loaded() {
     createScenary();
     createMPlayer();
     app.ticker.add(gameLoop);
+    app.ticker.maxFPS = 30
 }
-
 function createScenary() {
     //sky set
     sprites.sky.width = w;
@@ -92,76 +102,75 @@ function createScenary() {
 }
 function createPlayer() {
     // player set
-    sprites.player.width = w / 16;
-    sprites.player.height = h / 10;
-    sprites.player.x = app.view.width / 2;
-    sprites.player.y = app.view.height / 2; 
+    sprites.player.x = 30;
+    sprites.player.y =  236;
+    sprites.player.width = 50;
+    sprites.player.height = 35;
     app.stage.addChild(sprites.player);
 }
 function createMPlayer() {
 
-    mPlayer.x = w - 200
-    mPlayer.y = h - 900
+    mPlayer.x = 420
+    mPlayer.y = 20
 
     mPlayer.addChild(sprites.tray);
-    sprites.tray.width = 120;
-    sprites.tray.height = 45;
+    sprites.tray.width = 50;
+    sprites.tray.height = 20;
     
     //next
     mPlayer.addChild(sprites.next);
-    sprites.next.width = 25;
-    sprites.next.height = 25;
+    sprites.next.width = 9;
+    sprites.next.height = 10;
     sprites.next.interactive = true;
     sprites.next.buttonMode = true;
     sprites.next.on('pointerdown', nextTrack);
     
     //prev
     mPlayer.addChild(sprites.prev);
-    sprites.prev.width = 25;
-    sprites.prev.height = 25;
+    sprites.prev.width = 9;
+    sprites.prev.height = 10;
     sprites.prev.interactive = true;
     sprites.prev.buttonMode = true;
     sprites.prev.on('pointerdown', backTrack)
     
     //play / pause
     mPlayer.addChild(sprites.play);
-    sprites.play.width = 25;
-    sprites.play.height = 25;
+    sprites.play.width = 14;
+    sprites.play.height = 14;
     sprites.play.interactive = true;
     sprites.play.buttonMode = true;
     sprites.play.on('pointerdown', demute);
     
     mPlayer.addChild(sprites.disk);
-    sprites.disk.width = 60;
-    sprites.disk.height = 60;
+    sprites.disk.width = 24;
+    sprites.disk.height = 24;
     sprites.disk.anchor.set(0.5);
     sprites.disk.interactive = true;
     sprites.disk.buttonMode = true;
     sprites.disk.on('pointerdown', redirect);
 
-    sprites.tray.x = -20
-    sprites.tray.y = -10
+    sprites.tray.x = 0
+    sprites.tray.y = 0
     
-    sprites.prev.x = -5;
-    sprites.prev.y = 2;
+    sprites.prev.x = 5;
+    sprites.prev.y = 5;
 
-    sprites.play.x = 22;
-    sprites.play.y = 2;
+    sprites.play.x = 15;
+    sprites.play.y = 3;
 
-    sprites.next.x = 50;
-    sprites.next.y = 2;
+    sprites.next.x = 30;
+    sprites.next.y = 5;
 
-    sprites.disk.x = 110;
-    sprites.disk.y = 15;
+    sprites.disk.x = 53;
+    sprites.disk.y = 10;
 }
 
 let wind = new Howl ({
     src:['../sounds/windo.mp3'],
     autoplay: true,
-    volume: .3,
+    volume: 0,
     loop:true
 })
-
 
 let tracks = [
     new Howl({
@@ -202,7 +211,6 @@ let tracks = [
 let trackNum = 0;
 let trackMax = tracks.length - 1;   
 let paused = false;
-console.log(trackMax);
 tracks[trackNum].play();
 
 // mPlayer commands
@@ -265,39 +273,60 @@ let keys = {};
 window.addEventListener("keydown", (e) => { keys[e.key] = true });
 window.addEventListener("keyup", (e) => { keys[e.key] = false});
 
+let grv = 2;
+let groundHeight = 236; 
+let bboxRight = w - 11;
+let bboxLeft = 0;
+let vsp = 0;
+let hsp = 5;
+let acl = 0.2;
+
 function gameLoop() {
     sprites.disk.rotation += rotation;
 
-    // w
-    if (keys["w"]) {
-        /* if (!player.playing) {
-            player.textures = playerSheet.walkNorth;
-            player.play();
-        } */
-        sprites.player.y -= 5;
+    function onGround() {
+        if(sprites.player.y <= groundHeight) {
+            return false;
+        } else {
+            return true;
+        }
     }
-    // a
-    if (keys["a"]) {
-        /*  if (!player.playing) {
-            player.textures = playerSheet.walkWest;
-            player.play();
-        } */
-        sprites.player.x -= 5;
+
+    // a (left -1)
+    if (keys["a"] == true ) {
+        if (hsp <= -7) {
+            hsp = -7
+        } else { 
+            hsp -= acl;
+        }
+    } else if (keys["d"] == true) {
+        if (hsp >= 7) {
+            hsp = 7
+        } else { 
+            hsp += acl;
+        }
+    } else {
+        hsp = 0
     }
-    // s
-    if (keys["s"]) {
-        /* if (!player.playing) {
-            player.textures = playerSheet.walkSouth;
-            player.play();
-        } */
-        sprites.player.y += 5;
+
+    // gravity
+    if (!onGround() ) {  
+        vsp += grv; 
+    } else {
+        vsp = 0
     }
-    // d
-    if (keys["d"]) {
-        /* if (!player.playing) {
-            player.textures = playerSheet.walkEast;
-            player.play();
-        } */
-        sprites.player.x += 5;
+    //jump
+    if (keys[" "] == true && onGround()) {
+        vsp -= 10
     }
+    //test colision right and left
+    if ((sprites.player.x + hsp) >= bboxRight) {
+        hsp = 0
+    }
+    if ((sprites.player.x + hsp) <= bboxLeft) {
+        hsp = 0
+    }
+
+    sprites.player.y += vsp;
+    sprites.player.x += hsp;
 }
